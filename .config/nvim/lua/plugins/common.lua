5c75228ab04621c94a865c104f996e5fcb3a4d45
Chinedum Ukejianya <cukejianya@gmail.com>
2024-12-05 09:59:40 -0500

    Add marks and clean up common.lua

--- .config/nvim/lua/plugins/common.lua
+++ .config/nvim/lua/plugins/common.lua
@@ -1,11 +1,7 @@
 return {
-  { 'moll/vim-bbye', event = 'VeryLazy',  cmd = {'Bdelete', 'Bwipeout'}},
-  { 'tpope/vim-surround', event = 'VeryLazy'},
-  { 'junegunn/goyo.vim', event = 'VeryLazy', cmd = {'Goyo'}},
-  { 'navarasu/onedark.nvim', lazy = false },
-  {
-    'numToStr/Comment.nvim',
-    opts = {},
-  },
-  'lukas-reineke/indent-blankline.nvim',
+  { "moll/vim-bbye", event = "VeryLazy", cmd = { "Bdelete", "Bwipeout" } },
+  { "tpope/vim-surround", event = "VeryLazy" },
+  { "junegunn/goyo.vim", event = "VeryLazy", cmd = { "Goyo" } },
+  { "chentoast/marks.nvim" },
+  { "numToStr/Comment.nvim", opts = {} },
 }
