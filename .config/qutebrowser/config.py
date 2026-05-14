c2bb164785db3746df7a2a54e75ec6bf2cf84250
Chinedum Ukejianya <cukejianya@gmail.com>
2026-05-14 15:50:01 -0400

    Update qutebrowser config and quickmarks

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,15 +1,22 @@
 config.load_autoconfig(True)
 
 c.colors.webpage.darkmode.enabled = True 
+c.completion.use_best_match = True 
 
 c.editor.command = ['nvim', '-f', '{}']
+c.window.hide_decoration = True
 
 c.tabs.position = 'left'
 c.tabs.show = 'switching'
+c.url.auto_search = 'naive'
+c.fonts.default_size = '14pt'
 
 c.url.searchengines = {
     'DEFAULT': 'https://duckduckgo.com/?q={}',
-    'y': 'https://youtube.com/results?search_query={}'
+    'y': 'https://youtube.com/results?search_query={}',
+    'cs': 'https://codesearch.spotify.net/search?q={}',
+    'gm': 'https://www.google.com/maps?q={}',
+    'j': 'https://spotify.atlassian.net/browse/{}'
 }
 
 config.bind('<Alt-Shift-u>', 'spawn --userscript qute-keepassxc --key 177C5800F00A9FAB', mode='insert')
@@ -18,3 +25,6 @@ config.bind('pt', 'spawn --userscript qute-keepassxc --key 177C5800F00A9FAB --to
 
 config.unbind('d')
 config.bind('x', 'tab-close')
+
+config.bind('<Ctrl-O>', 'tab-focus stack-prev')
+config.bind('<Ctrl-I>', 'tab-focus stack-next')
